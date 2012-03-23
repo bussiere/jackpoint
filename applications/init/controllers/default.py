30d2066088b3bc660b590cd6503f77dad957d939
bussiere <bussiere@gmail.com>
2012-03-23 10:51:29 +0100

    decommentyage

--- applications/init/controllers/default.py
+++ applications/init/controllers/default.py
@@ -140,55 +140,55 @@ from gluon.storage import Storage
 
 
 
-# def ensurefirstuser(firstname,lastname,email,password)​:
-#     users = db(db.auth_user.email==email).​select()
-#     if users:
-#         user_id = users[0].id
-#         created = False
-#         if settings.debug_ensure_first_​user == True:
-#             print ('found user_id so created equals %s') % created
-#         return (user_id,created)
+def ensurefirstuser(firstname,lastname,email,password)​:
+    users = db(db.auth_user.email==email).​select()
+    if users:
+        user_id = users[0].id
+        created = False
+        if settings.debug_ensure_first_​user == True:
+            print ('found user_id so created equals %s') % created
+        return (user_id,created)
 
-#     else:
-#         my_crypt = CRYPT(key=auth.settings.hmac_​key)
-#         crypt_pass = my_crypt(password)[0]        
-#         id_user= db.auth_user.insert(
-#                               ​     first_name=firstname,
-#                               ​     last_name=lastname,
-#                               ​     email = email,
-#                               ​     password = crypt_pass
-#                               ​     )
-#         created = True
-#         if settings.debug_ensure_first_​user == True:
-#             print ('creating user_id')
-#         return (id_user,created)
+    else:
+        my_crypt = CRYPT(key=auth.settings.hmac_​key)
+        crypt_pass = my_crypt(password)[0]        
+        id_user= db.auth_user.insert(
+                              ​     first_name=firstname,
+                              ​     last_name=lastname,
+                              ​     email = email,
+                              ​     password = crypt_pass
+                              ​     )
+        created = True
+        if settings.debug_ensure_first_​user == True:
+            print ('creating user_id')
+        return (id_user,created)
 
         
 
 
-# def __ensure_users():
-#     user_id, created = _ensure_user('first','last','e​mail@address.com','​UserPassword12345')
-#     if settings.debug_ensure_users == True:
-#         print ('user id %i') % user_id
-#         print ('created %s') % created
+def __ensure_users():
+    user_id, created = _ensure_user('first','last','e​mail@address.com','​UserPassword12345')
+    if settings.debug_ensure_users == True:
+        print ('user id %i') % user_id
+        print ('created %s') % created
 
-# def __ensure_group_role(role,​description):
-#     if not auth.id_group(role=role):    
-#         auth.add_group(role=role,​description=description)    
-#         if settings.debug_ensure_group_​role == True:
-#             print ('created role %s') %(role)
+def __ensure_group_role(role,​description):
+    if not auth.id_group(role=role):    
+        auth.add_group(role=role,​description=description)    
+        if settings.debug_ensure_group_​role == True:
+            print ('created role %s') %(role)
 
-# def __ensure_membership(group_id, user_id):
-#     return_group_id = auth.add_membership(role=role, user_id=user_id)
-#     if settings.debug_ensure_​membership == True:
-#         print ('made user_id member of %s') %(role)
-#     return return_group_id
+def __ensure_membership(group_id, user_id):
+    return_group_id = auth.add_membership(role=role, user_id=user_id)
+    if settings.debug_ensure_​membership == True:
+        print ('made user_id member of %s') %(role)
+    return return_group_id
  
-# def __ensure_permissions(role, description, user_id):
-#     __ensure_group_role(role,​description)
-#     return_group_id = __ensure_membership(group_id, user_id)
-#     if settings.debug_ensure_​permissions == True:
-#         print return_group_id
+def __ensure_permissions(role, description, user_id):
+    __ensure_group_role(role,​description)
+    return_group_id = __ensure_membership(group_id, user_id)
+    if settings.debug_ensure_​permissions == True:
+        print return_group_id
 
 
 def index():
