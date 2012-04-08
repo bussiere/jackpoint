74ee71ca30952a9d63d47b58ed11a90df789edce
bussiere <bussiere@gmail.com>
2012-04-08 17:16:31 +0200

    modifications des forms.py Bussiere
    Signed-off-by: bussiere <bussiere@gmail.com>

--- jackpoint/jackpoint/item/forms.py
+++ jackpoint/jackpoint/item/forms.py
@@ -11,5 +11,6 @@ from django.forms.extras.widgets import SelectDateWidget
 class ItemForm(forms.Form):
    
     PRIVATE_CHOICES = (('0', 'Private'), ('1', 'Public'))
+    type = forms.CharField(widget=forms.HiddenInput(),default="item")
     Possede = ChoiceField(label='Possede : ',widget=CheckboxSelectMultiple)
     private = ChoiceField(label='Visibility : ',widget=RadioSelect, choices=PRIVATE_CHOICES)
--- jackpoint/jackpoint/skill/forms.py
+++ jackpoint/jackpoint/skill/forms.py
@@ -14,6 +14,7 @@ class SkillForm(forms.Form):
     parent = forms.CharField(widget=forms.HiddenInput())
     child = forms.CharField(widget=forms.HiddenInput())
     niveau = forms.CharField(widget=forms.HiddenInput())
+    type = forms.CharField(widget=forms.HiddenInput(),default="skill")
     CARAC_CHOICES = (('1', 'Ne la possede pas'),('1', 'Nul'), ('2', 'Bof'),('3', 'Moyen'),('4', 'Bon'),('5', 'Excellent'))
     PRIVATE_CHOICES = (('0', 'Private'), ('1', 'Public'))
     level = ChoiceField(label='Niveau : ',widget=RadioSelect, choices=CARAC_CHOICES)
