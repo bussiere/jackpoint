f31cc4182b9cecfcc7d2bcc234adcc664f6b4244
bussiere <bussiere@gmail.com>
2012-04-08 16:02:57 +0200

    on va tenter de trier les skills ou pas ... quoique on peut aussi essayer de ne pas trier et partir directement sur les coups de mains. Bussiere

--- jackpoint/jackpoint/skill/forms.py
+++ jackpoint/jackpoint/skill/forms.py
@@ -11,6 +11,9 @@ from django.forms.extras.widgets import SelectDateWidget
 class SkillForm(forms.Form):
     carac = forms.CharField(widget=forms.HiddenInput())
     id = forms.CharField(widget=forms.HiddenInput())
+    parent = forms.CharField(widget=forms.HiddenInput())
+    child = forms.CharField(widget=forms.HiddenInput())
+    niveau = forms.CharField(widget=forms.HiddenInput())
     CARAC_CHOICES = (('1', 'Ne la possede pas'),('1', 'Nul'), ('2', 'Bof'),('3', 'Moyen'),('4', 'Bon'),('5', 'Excellent'))
     PRIVATE_CHOICES = (('0', 'Private'), ('1', 'Public'))
     level = ChoiceField(label='Niveau : ',widget=RadioSelect, choices=CARAC_CHOICES)
